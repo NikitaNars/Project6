0e6d00d33712530e44c70b51ab8b83309d138a06
Nikita Narseev <nne150307@gmail.com>
2024-10-02 14:50:42 +0500

    Добавьте файлы проекта.

---
+++ Project6/arr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	srand(time(NULL));
+	const int size = 20;
+	int arr[size];
+	
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 6 + 5;
+		if (i == 10)
+		{
+			std::cout << std::endl;
+		}
+		std::cout << arr[i] << " ";
+	}
+
+}
